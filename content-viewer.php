9c223047bf028c2b5fb64d73a501c0343b92688d
Caleb_Grove <caleb@grovedesign.co>
2017-03-25 19:36:07 -0700

    Create the page if it doesn’t exist.

--- content-viewer.php
+++ content-viewer.php
@@ -2,4 +2,14 @@
 $kirby->set('widget', 'widget', __DIR__ . '/widget');
 $kirby->set('blueprint', 'content-viewer', __DIR__ . '/blueprints/content-viewer.yml');
 $kirby->set('template', 'content-viewer', __DIR__ . '/templates/content-viewer.php');
+
+
+
+// If the page doesn't exist yet, create it.
+if(!site()->index()->findBy('intendedTemplate', 'content-viewer')) {
+	site()->create('content-viewer', 'content-viewer', array(
+		'title' => '',
+		'text' => ''
+	));
+}
 ?>
